b817e8d7aae536806f8895d821417b0fe36c97c3
Nayan <n007kingkong@gmail.com>
2025-08-31 00:27:51 +0600

    filter method

---
+++ filter.js
@@ -0,0 +1,10 @@
+
+const ages = [10, 56, 39, 48, 63]
+
+const old = ages.filter(age => age>40 )
+// console.log(old);
+
+const numbers = [55, 67, 34, 78, 90]
+
+const isEven = numbers.filter(num => num%2===0)
+console.log(isEven)
