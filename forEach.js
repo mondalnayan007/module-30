9e7c749a629db369608bcf2536878659be221317
Nayan <n007kingkong@gmail.com>
2025-08-30 23:55:38 +0600

    forEach method

---
+++ forEach.js
@@ -0,0 +1,10 @@
+
+const numbers = [10, 56, 39, 48, 63]
+
+//  numbers.forEach(x => console.log(x))
+
+const friends = [
+    "tamim",{name:"sakib",age:35,district:"Dhaka"},"musfik"
+]
+
+friends.forEach(friend => console.log(friend));
